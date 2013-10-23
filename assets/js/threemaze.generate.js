75f2673a802bd1ca756f96e3bd98f023fd41ea72
Johan Satgé <jsatge@tequilarapido.com>
2013-10-23 09:07:19 +0200

    Code clean, refactoring camera calculations

---
+++ assets/js/threemaze.generate.js
@@ -0,0 +1,84 @@
+
+/**
+ * Generates a maze
+ * Raw copy paste from http://www.roguebasin.roguelikedevelopment.org/index.php?title=Simple_maze#Maze_Generator_in_Javascript
+ * @param size
+ */
+threemaze.prototype.generateMaze = function(size)
+{
+    var cN = [[0,0],[0,0],[0,0],[0,0]];
+    var cx;
+    var cy;
+    var map = [];
+    var random_direction, int_done = 0;
+    for (var x = 1; x <= size; x += 1)
+    {
+        map[x] = [];
+        for (var y = 1; y <= size; y += 1)
+        {
+            map[x][y] = 0;
+        }
+    }
+    do
+    {
+        x= 2 + Math.floor(Math.random() * (size - 1));
+        if (x % 2 != 0)
+        {
+            x -= 1;
+        }
+        y= 2 + Math.floor(Math.random() * (size - 1));
+        if (y % 2 != 0)
+        {
+            y -= 1;
+        }
+        if (int_done == 0)
+        {
+            map[x][y] = 1;
+        }
+        if (map[x][y] == 1)
+        {
+            random_direction = Math.floor(Math.random() * 4);
+            if (random_direction == 0)
+            {
+                cN = [[-1,0],[1,0],[0,-1],[0,1]];
+            }
+            else if (random_direction == 1)
+            {
+                cN = [[0,1],[0,-1],[1,0],[-1,0]];
+            }
+            else if (random_direction == 2)
+            {
+                cN = [[0,-1],[0,1],[-1,0],[1,0]];
+            }
+            else if (random_direction == 3)
+            {
+                cN = [[1,0],[-1,0],[0,1],[0,-1]];
+            }
+            bln_blocked = 1;
+            do
+            {
+                bln_blocked += 1;
+                for (var int_dir = 0; int_dir <= 3; int_dir += 1)
+                {
+                    cx = x + cN[int_dir][0] * 2;
+                    cy = y + cN[int_dir][1] * 2;
+                    if (cx < size && cy < size && cx > 1 && cy > 1)
+                    {
+                        if (map[cx][cy] != 1)
+                        {
+                            map[cx][cy] = 1;
+                            map[x][y] = 1;
+                            map[x + cN[int_dir][0]][y + cN[int_dir][1]] = 1;
+                            x = cx;
+                            y = cy;
+                            bln_blocked = 0;
+                            int_done += 1;
+                            int_dir = 4;
+                        }
+                    }
+                }
+            } while (bln_blocked == 1)
+        }
+    } while (int_done + 1 < ((size - 1) * (size - 1)) / 4)
+    return map;
+};
--- assets/js/threemaze.js
+++ assets/js/threemaze.js
@@ -1,4 +1,8 @@
 
+/**
+ * Maze class
+ * @param $element
+ */
 function threemaze($element)
 {
     // Object attributes
@@ -7,6 +11,8 @@ function threemaze($element)
     this.cameraHelper =     {};
     this.scene =            {};
     this.renderer =         {};
+    this.side =             21;
+    this.thickness =        20;
 
     // Events
     this.$element.on('mousemove', $.proxy(this,'onMouseMove'));
@@ -29,18 +35,16 @@ function threemaze($element)
  */
 threemaze.prototype.initObjects = function()
 {
-    var the_size = 21;
-    var map = this.generateMaze(the_size);
-    for (var x = 1; x < the_size + 1; x += 1)
+    var map = this.generateMaze(this.side);
+    for (var x = 1; x < this.side + 1; x += 1)
     {
-        for (var y = 1;y < the_size + 1; y += 1)
+        for (var y = 1;y < this.side + 1; y += 1)
         {
             if (map[x][y] == 0)
             {
-                var wall_geometry = new THREE.CubeGeometry(20, 30, 20, 1, 1, 1);
-                var material = new THREE.MeshLambertMaterial({color: 0xff99ff, wireframe: false});
-                var wall_mesh = new THREE.Mesh(wall_geometry, material);
-                wall_mesh.position.set(x * 20 - ((the_size * 20) / 2), 15, y * 20 - ((the_size * 20) / 2));
+                var wall_geometry =     new THREE.CubeGeometry(this.thickness, this.thickness, this.thickness, 1, 1, 1);
+                var wall_mesh =         new THREE.Mesh(wall_geometry, new THREE.MeshLambertMaterial({color: 0xffffff, wireframe: false}));
+                wall_mesh.position.set(x * this.thickness - ((this.side * this.thickness) / 2), this.thickness / 2, y * 20 - ((this.side * this.thickness) / 2));
                 this.scene.add(wall_mesh);
             }
         }
@@ -56,29 +60,24 @@ threemaze.prototype.initScene = function()
     this.scene = new THREE.Scene();
 
     // Camera
-    this.camera =               new THREE.PerspectiveCamera(45, 1, 1, 2000);
-    this.camera.angles =        {horizontal: 0, vertical: 0};
-    this.camera.clicked =       false;
+    this.camera =            new THREE.PerspectiveCamera(45, 1, 1, 2000);
+    this.camera.angles =     {horizontal: 0, vertical: 0};
+    this.camera.clicked =    false;
 
     // Lights
-    var ambient = new THREE.AmbientLight(0x999999);
-    this.scene.add(ambient);
+    this.scene.add(new THREE.AmbientLight(0x999999));
     var directional = new THREE.DirectionalLight(0xffffff, 0.5);
     directional.position.set(0, 0.5, 1);
     this.scene.add(directional);
 
-    // Materials
-    var black_material = new THREE.MeshBasicMaterial({color: 0x000000, wireframe: false});
-    var white_material = new THREE.MeshLambertMaterial({color: 0xe3ded0, wireframe: false});
-    this.materials = {'atomes': black_material, 'centre': white_material};
-
     // Camera helper
     var geometry =  new THREE.Geometry();
     var material =  new THREE.LineBasicMaterial({color: 0x333333, lineWidth: 1});
-    geometry.vertices.push(new THREE.Vector3(0,0,0), new THREE.Vector3(500,0,0));
+    geometry.vertices.push(new THREE.Vector3(0, 0, 0), new THREE.Vector3(Math.sqrt(3) * (this.side * this.thickness)), 0, 0);
     this.cameraHelper = new THREE.Line(geometry, material);
     this.scene.add(this.cameraHelper);
-    this.cameraHelper.rotation.z = Math.PI / 5;
+    this.cameraHelper.visible = false;
+    this.cameraHelper.targetRotation = false;
 
     // Renderer
     this.renderer = typeof WebGLRenderingContext != 'undefined' && window.WebGLRenderingContext ? new THREE.WebGLRenderer({antialias: true}) : new THREE.CanvasRenderer({});
@@ -86,19 +85,25 @@ threemaze.prototype.initScene = function()
 };
 
 /**
- * Moving the mouse over the container
+ * Moving the mouse over the container: sets a target rotation for the camera helper
  * @param evt
  */
 threemaze.prototype.onMouseMove = function(evt)
 {
     if (this.camera.clicked !== false)
     {
-        this.cameraHelper.rotation.y += (this.camera.clicked.x - evt.pageX) / 5000;
-        this.cameraHelper.rotation.z += (evt.pageY - this.camera.clicked.y) / 5000;
-
-        var camera_position = this.cameraHelper.geometry.vertices[1].clone().applyProjection(this.cameraHelper.matrixWorld);
-        this.camera.position = camera_position;
-        this.camera.lookAt(this.scene.position);
+        var target_rotation = {};
+        target_rotation.z = this.cameraHelper.rotation.z + ((evt.pageY - this.camera.clicked.y) / 800);
+        if (target_rotation.z < 0)
+        {
+            target_rotation.z = 0;
+        }
+        if (target_rotation.z > (Math.PI / 2) - 0.1)
+        {
+            target_rotation.z = Math.PI / 2 - 0.1;
+        }
+        target_rotation.y = this.cameraHelper.rotation.y + ((this.camera.clicked.x - evt.pageX) / 800);
+        this.cameraHelper.targetRotation = target_rotation;
     }
 };
 
@@ -124,10 +129,19 @@ threemaze.prototype.onMouseUp = function(evt)
 
 /**
  * Render loop
+ * Sets the camera position and renders the scene
  */
 threemaze.prototype.render = function()
 {
     requestAnimationFrame($.proxy(this, 'render'));
+    if (this.cameraHelper.targetRotation !== false)
+    {
+        this.cameraHelper.rotation.z += (this.cameraHelper.targetRotation.z - this.cameraHelper.rotation.z) / 10;
+        this.cameraHelper.rotation.y += (this.cameraHelper.targetRotation.y - this.cameraHelper.rotation.y) / 10;
+        var camera_position = this.cameraHelper.geometry.vertices[1].clone().applyProjection(this.cameraHelper.matrixWorld);
+        this.camera.position = camera_position;
+        this.camera.lookAt(this.scene.position);
+    }
     this.renderer.render(this.scene, this.camera);
 };
 
@@ -140,83 +154,4 @@ threemaze.prototype.onWindowResize = function()
     this.renderer.setSize($window.width(), $window.height());
     this.camera.aspect = $window.width() / $window.height();
     this.camera.updateProjectionMatrix();
-};
-
-/**
- * Generates a maze
- * Raw copy paste from http://www.roguebasin.roguelikedevelopment.org/index.php?title=Simple_maze#Maze_Generator_in_Javascript
- * @todo rewrite this
- * @param size
- */
-threemaze.prototype.generateMaze = function(size)
-{
-    // Coordinates of N(x,y) S(x,y) E(x,y) W(x,y)
-    // it's necessary to have one of each so that if one direction
-    // doesn't work properly, we can try each direction until one
-    // works, or that we know there isn't a location possible.
-    var cN = [[0,0],[0,0],[0,0],[0,0]];
-    var x,y,cx,cy;
-    var map = new Array(size);
-    var randomDir,intDone=0;
-    for (var i = 0; i <= size * size; ++i)
-    {
-        map[i] = new Array(size);
-    }
-    // Initialize the Map Array to Zeros
-    for (x = 1; x <= size; ++x)
-    {
-        for (y = 1; y <= size; ++y)
-        {
-            map[x][y] = 0;
-        }
-    }
-    do
-    {
-        // Roll random x's and y's and make sure the value is odd
-        x=2+Math.floor(Math.random()*(size-1));if (x%2!=0) --x;
-        y=2+Math.floor(Math.random()*(size-1));if (y%2!=0) --y;
-        // Ensure that the first random map location starts the process
-        if (intDone==0) map[x][y]=1;
-        if (map[x][y]==1){
-            //Randomize Directions
-            randomDir=Math.floor(Math.random()*4)
-            if (randomDir==0){
-                cN = [[-1,0],[1,0],[0,-1],[0,1]];
-            } else if (randomDir==1){
-                cN = [[0,1],[0,-1],[1,0],[-1,0]];
-            } else if (randomDir==2){
-                cN = [[0,-1],[0,1],[-1,0],[1,0]];
-            } else if (randomDir==3){
-                cN = [[1,0],[-1,0],[0,1],[0,-1]];
-            } //end if
-            blnBlocked=1;
-            do {
-                blnBlocked++;
-                for (var intDir=0; intDir<=3; ++intDir){
-                    // Determine which direction the tile is
-                    cx=x+cN[intDir][0]*2;
-                    cy=y+cN[intDir][1]*2;
-                    //Check to see if the tile can be used
-                    if (cx<size && cy<size && cx>1 && cy>1){
-                        if (map[cx][cy]!=1){
-                            //create destination location
-                            map[cx][cy]=1;
-                            //create current location
-                            map[x][y]=1;
-                            //create inbetween location
-                            map[x+cN[intDir][0]][y+cN[intDir][1]]=1;
-                            //set destination location to current
-                            x=cx;y=cy;
-                            blnBlocked=0;
-                            intDone++;
-                            intDir=4
-                        } //end if
-                    } //end if
-                } //end for
-                //recursive, no directions found, loop back a node
-            } while (blnBlocked==1) //end do
-        } //end if
-    } while (intDone+1<((size-1)*(size-1))/4) //end do
-
-    return map;
 };
